skrylnikov/z-torrent

commit 3f50bedc4e0c0054ad47bd58c2847e5a95779c96

wait to notify() or updateInterest() at end of GC
To prevent it from possibly being called twice, which will do extra work

packages/webtorrent/lib/torrent.js
@@ -893,7 +893,7 @@ Torrent.prototype.deselect = function (start, end, priority) {
   for (var i = 0; i < self._selections.length; ++i) {
     var s = self._selections[i]
     if (s.from === start && s.to === end && s.priority === priority) {
-      self._selections.splice(i--, 1)
+      self._selections.splice(i, 1)
       break
     }
   }
@@ -1131,13 +1131,16 @@ Torrent.prototype._updateSelections = function () {
 Torrent.prototype._gcSelections = function () {
   var self = this
 
-  for (var i = 0; i < self._selections.length; i++) {
+  var i
+  var toRemove = []
+
+  for (i = 0; i < self._selections.length; ++i) {
     var s = self._selections[i]
     var oldOffset = s.offset
 
     // check for newly downloaded pieces in selection
     while (self.bitfield.get(s.from + s.offset) && s.from + s.offset < s.to) {
-      s.offset++
+      s.offset += 1
     }
 
     if (oldOffset !== s.offset) s.notify()
@@ -1145,8 +1148,16 @@ Torrent.prototype._gcSelections = function () {
     if (!self.bitfield.get(s.from + s.offset)) continue
 
     // remove fully downloaded selection
-    self._selections.splice(i--, 1) // decrement i to offset splice
-    s.notify() // TODO: this may notify twice in a row. is this a problem?
+    toRemove.push(i)
+  }
+
+  if (toRemove.length > 0) {
+    // Since self._selections is being modified in-place, it's necessary to
+    // traverse `toRemove` in reverse.
+    for (i = toRemove.length - 1; i >= 0; --i) {
+      self._selections.splice(i, 1)
+    }
+    s.notify()
     self._updateInterest()
   }
 

commit cce4df2fd6d7b84676d747bcc13da8ee9abe0b61

Fix files under 16Kb are not downloaded correctly
Fixes: https://github.com/feross/webtorrent/issues/1077

Bug originally introduced in:
https://github.com/feross/webtorrent/commit/3f50bedc4e0c0054ad47bd58c284
5bd91fc36233?diff=split

packages/webtorrent/lib/torrent.js
@@ -1131,10 +1131,7 @@ Torrent.prototype._updateSelections = function () {
 Torrent.prototype._gcSelections = function () {
   var self = this
 
-  var i
-  var toRemove = []
-
-  for (i = 0; i < self._selections.length; ++i) {
+  for (var i = 0; i < self._selections.length; ++i) {
     var s = self._selections[i]
     var oldOffset = s.offset
 
@@ -1147,16 +1144,9 @@ Torrent.prototype._gcSelections = function () {
     if (s.to !== s.from + s.offset) continue
     if (!self.bitfield.get(s.from + s.offset)) continue
 
-    // remove fully downloaded selection
-    toRemove.push(i)
-  }
+    self._selections.splice(i, 1) // remove fully downloaded selection
+    i -= 1 // decrement i to offset splice
 
-  if (toRemove.length > 0) {
-    // Since self._selections is being modified in-place, it's necessary to
-    // traverse `toRemove` in reverse.
-    for (i = toRemove.length - 1; i >= 0; --i) {
-      self._selections.splice(i, 1)
-    }
     s.notify()
     self._updateInterest()
   }